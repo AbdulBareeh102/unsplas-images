8bdec1ffba418af47cf7a58c3dc78bb4e4e4596b
AbdulBareeh102 <saadudeenadebayo621@gmail.com>
2023-10-06 11:36:30 +0100

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearcForm";
+import ThemeToggle from "./ThemeToggle";
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
